f3ed29970b6cd8b664e1b2efff84524728950848
Marian13 <mariankostyk13895@gmail.com>
2025-06-24 16:15:40 +0300

    chore(ruby_arguments): extract Support::Arguments class from ConveientService since it is mature enought to be independent gem

--- lib/ruby_arguments.rb
+++ lib/ruby_arguments.rb
@@ -1,4 +1,182 @@
 # frozen_string_literal: true
 
 class RubyArguments
+  module Exceptions
+    class Base < ::StandardError
+    end
+
+    class InvalidKeyType < Base
+      class << self
+        ##
+        # @api private
+        # @param key [Object] Can be any type.
+        # @return [RubyArguments::Exceptions::InvalidKeyType]
+        #
+        def create(key:)
+          message = <<~TEXT
+            `#[]` accepts only `Integer` and `String` keys.
+
+            Key `#{key.inspect}` has `#{key.class}` class.
+          TEXT
+
+          new(message)
+        end
+      end
+    end
+  end
+
+  class NullArguments < ::RubyArguments
+    ##
+    # @api private
+    # @return [void]
+    #
+    def initialize
+      @args = []
+      @kwargs = {}
+      @block = nil
+    end
+
+    ##
+    # @api public
+    # @return [Boolean]
+    #
+    def null_arguments?
+      true
+    end
+  end
+
+  ##
+  # @api public
+  # @!attribute [r] args
+  #   @return [Array<Object>]
+  #
+  attr_reader :args
+
+  ##
+  # @api public
+  # @!attribute [r] kwargs
+  #   @return [Hash{Symbol => Object}]
+  #
+  attr_reader :kwargs
+
+  ##
+  # @api public
+  # @!attribute [r] block
+  #   @return [Proc, nil]
+  #
+  attr_reader :block
+
+  ##
+  # @api public
+  # @param args [Array<Object>]
+  # @param kwargs [Hash{Symbol => Object}]
+  # @param block [Proc, nil]
+  # @return [void]
+  #
+  def initialize(*args, **kwargs, &block)
+    @args = args
+    @kwargs = kwargs
+    @block = block
+  end
+
+  class << self
+    ##
+    # @api public
+    # @return [RubyArguments::NullArguments]
+    #
+    def null_arguments
+      @null_arguments ||= ::RubyArguments::NullArguments.new
+    end
+  end
+
+  ##
+  # @api public
+  # @return [Boolean]
+  #
+  def null_arguments?
+    false
+  end
+
+  ##
+  # @api public
+  # @return [Booleam]
+  #
+  def any?
+    return true if args.any?
+    return true if kwargs.any?
+    return true if block
+
+    false
+  end
+
+  ##
+  # @api public
+  # @return [Booleam]
+  #
+  def none?
+    !any?
+  end
+
+  ##
+  # @api public
+  # @param key [Integer, Symbol]
+  # @return [Object] Can be any type.
+  # @raise [RubyArguments::Exceptions::InvalidKeyType]
+  #
+  def [](key)
+    case key
+    when ::Integer then args[key]
+    when ::Symbol then kwargs[key]
+    else raise Exceptions::InvalidKeyType.create(key: key)
+    end
+  end
+
+  ##
+  # @api public
+  # @param other [Object]
+  # @return [Boolean, nil]
+  #
+  def ==(other)
+    return unless other.instance_of?(self.class)
+
+    return false if args != other.args
+    return false if kwargs != other.kwargs
+    return false if block != other.block
+
+    true
+  end
+
+  ##
+  # @api public
+  # @return [Array]
+  # @note https://zverok.space/blog/2022-12-20-pattern-matching.html
+  # @note https://ruby-doc.org/core-2.7.2/doc/syntax/pattern_matching_rdoc.html
+  # @note Expected to be called only from pattern matching. Avoid direct usage of this method.
+  #
+  def deconstruct
+    [args, kwargs, block]
+  end
+
+  ##
+  # @api public
+  # @param keys [Array<Symbol>, nil]
+  # @return [Hash]
+  # @note https://zverok.space/blog/2022-12-20-pattern-matching.html
+  # @note https://ruby-doc.org/core-2.7.2/doc/syntax/pattern_matching_rdoc.html
+  # @note Expected to be called only from pattern matching. Avoid direct usage of this method.
+  #
+  def deconstruct_keys(keys)
+    keys ||= [:args, :kwargs, :block]
+
+    keys.each_with_object({}) do |key, hash|
+      case key
+      when :args
+        hash[key] = args
+      when :kwargs
+        hash[key] = kwargs
+      when :block
+        hash[key] = block
+      end
+    end
+  end
 end
--- spec/lib/ruby_arguments_spec.rb
+++ spec/lib/ruby_arguments_spec.rb
@@ -1,4 +1,299 @@
 # frozen_string_literal: true
 
 RSpec.describe RubyArguments do
+  let(:arguments) { described_class.new(*args, **kwargs, &block) }
+
+  let(:args) { [:foo] }
+  let(:kwargs) { {foo: :bar} }
+  let(:block) { proc { :foo } }
+
+  example_group "attributes" do
+    specify { expect(arguments.args).to eq(args) }
+    specify { expect(arguments.kwargs).to eq(kwargs) }
+    specify { expect(arguments.block).to eq(block) }
+  end
+
+  example_group "class methods" do
+    describe "#new" do
+      context "when `args` are NOT passed" do
+        let(:arguments) { described_class.new(**kwargs, &block) }
+
+        it "defaults `args` to empty array" do
+          expect(arguments.args).to eq([])
+        end
+      end
+
+      context "when `kwargs` are NOT passed" do
+        let(:arguments) { described_class.new(*args, &block) }
+
+        it "defaults `kwargs` to empty hash" do
+          expect(arguments.kwargs).to eq({})
+        end
+      end
+
+      context "when `block` is NOT passed" do
+        let(:arguments) { described_class.new(*args, **kwargs) }
+
+        it "defaults `block` to `nil`" do
+          expect(arguments.block).to be_nil
+        end
+      end
+    end
+  end
+
+  example_group "instance methods" do
+    describe "#null_arguments?" do
+      it "returns `false`" do
+        expect(arguments.null_arguments?).to eq(false)
+      end
+    end
+
+    describe "#any?" do
+      context "when arguments have at least one arg" do
+        let(:arguments) { described_class.new(*args) }
+
+        it "returns `true`" do
+          expect(arguments.any?).to eq(true)
+        end
+      end
+
+      context "when arguments have at least one kwarg" do
+        let(:arguments) { described_class.new(**kwargs) }
+
+        it "returns `true`" do
+          expect(arguments.any?).to eq(true)
+        end
+      end
+
+      context "when arguments have block" do
+        let(:arguments) { described_class.new(&block) }
+
+        it "returns `true`" do
+          expect(arguments.any?).to eq(true)
+        end
+      end
+
+      context "when arguments do NOT have args, kwargs and block" do
+        let(:arguments) { described_class.new }
+
+        it "returns `false`" do
+          expect(arguments.any?).to eq(false)
+        end
+      end
+    end
+
+    describe "#none?" do
+      context "when arguments have at least one arg" do
+        let(:arguments) { described_class.new(*args) }
+
+        it "returns `false`" do
+          expect(arguments.none?).to eq(false)
+        end
+      end
+
+      context "when arguments have at least one kwarg" do
+        let(:arguments) { described_class.new(**kwargs) }
+
+        it "returns `false`" do
+          expect(arguments.none?).to eq(false)
+        end
+      end
+
+      context "when arguments have at block" do
+        let(:arguments) { described_class.new(&block) }
+
+        it "returns `false`" do
+          expect(arguments.none?).to eq(false)
+        end
+      end
+
+      context "when arguments do NOT have args, kwargs and block" do
+        let(:arguments) { described_class.new }
+
+        it "returns `true`" do
+          expect(arguments.none?).to eq(true)
+        end
+      end
+    end
+
+    describe "#[]" do
+      context "when `key` is NOT valid" do
+        let(:key) { "abc" }
+
+        let(:exception_message) do
+          <<~TEXT
+            `#[]` accepts only `Integer` and `String` keys.
+
+            Key `#{key.inspect}` has `#{key.class}` class.
+          TEXT
+        end
+
+        it "raises `RubyArguments::Exceptions::InvalidKeyType`" do
+          expect { arguments[key] }
+            .to raise_error(RubyArguments::Exceptions::InvalidKeyType)
+            .with_message(exception_message)
+        end
+      end
+
+      context "when `key` is valid" do
+        context "when `key` is integer" do
+          let(:key) { 0 }
+
+          it "returns arg by `key` (index)" do
+            expect(arguments[key]).to eq(arguments.args[key])
+          end
+        end
+
+        context "when `key` is symbol" do
+          let(:key) { :foo }
+
+          it "returns kwarg by `key`" do
+            expect(arguments[key]).to eq(arguments.kwargs[key])
+          end
+        end
+      end
+    end
+
+    example_group "comparison" do
+      describe "#==" do
+        subject(:arguments) { described_class.new(*args, **kwargs, &block) }
+
+        context "when `other` has different class" do
+          let(:other) { 42 }
+
+          it "returns `false`" do
+            expect(arguments == other).to be_nil
+          end
+        end
+
+        context "when `other` has different `args`" do
+          let(:other) { described_class.new(:bar, **kwargs, &block) }
+
+          it "returns `false`" do
+            expect(arguments == other).to eq(false)
+          end
+        end
+
+        context "when `other` has different `kwargs`" do
+          let(:other) { described_class.new(*args, {bar: :baz}, &block) }
+
+          it "returns `false`" do
+            expect(arguments == other).to eq(false)
+          end
+        end
+
+        context "when `other` has different `block`" do
+          let(:other) { described_class.new(*args, **kwargs, &other_block) }
+          let(:other_block) { proc { :bar } }
+
+          it "returns `false`" do
+            expect(arguments == other).to eq(false)
+          end
+        end
+
+        context "when `other` has same attributes" do
+          let(:other) { described_class.new(*args, **kwargs, &block) }
+
+          it "returns `true`" do
+            expect(arguments == other).to eq(true)
+          end
+        end
+      end
+    end
+
+    describe "#deconstruct" do
+      let(:arguments) { described_class.new(*args, **kwargs, &block) }
+
+      let(:args) { [:foo] }
+      let(:kwargs) { {foo: :bar} }
+      let(:block) { proc { :foo } }
+
+      it "returns array with `args`, `kwargs` and `block`" do
+        expect(arguments.deconstruct).to eq([args, kwargs, block])
+      end
+    end
+
+    describe "#deconstruct_keys" do
+      let(:arguments) { described_class.new(*args, **kwargs, &block) }
+
+      let(:args) { [:foo] }
+      let(:kwargs) { {foo: :bar} }
+      let(:block) { proc { :foo } }
+
+      context "when `keys` is NOT `nil`" do
+        context "when `keys` is array with one key" do
+          context "when `keys` is array with `:args` key" do
+            it "returns hash with only `:args` key" do
+              expect(arguments.deconstruct_keys([:args])).to eq({args: args})
+            end
+          end
+
+          context "when `keys` is array with `:kwargs` key" do
+            it "returns hash with only `:kwargs` key" do
+              expect(arguments.deconstruct_keys([:kwargs])).to eq({kwargs: kwargs})
+            end
+          end
+
+          context "when `keys` is array with `:block` key" do
+            it "returns hash with only `:block` key" do
+              expect(arguments.deconstruct_keys([:block])).to eq({block: block})
+            end
+          end
+        end
+
+        context "when `keys` is array with multiple keys" do
+          it "returns hash with only those multiple keys" do
+            expect(arguments.deconstruct_keys([:args, :kwargs])).to eq({args: args, kwargs: kwargs})
+            expect(arguments.deconstruct_keys([:args, :block])).to eq({args: args, block: block})
+            expect(arguments.deconstruct_keys([:kwargs, :block])).to eq({kwargs: kwargs, block: block})
+          end
+        end
+      end
+
+      context "when `keys` is `nil`" do
+        it "returns hash with all possible keys" do
+          expect(arguments.deconstruct_keys(nil)).to eq({args: args, kwargs: kwargs, block: block})
+        end
+      end
+    end
+  end
+end
+
+RSpec.describe RubyArguments::Exceptions do
+  specify { expect(described_class::Base.superclass).to eq(StandardError) }
+  specify { expect(described_class::InvalidKeyType.superclass).to eq(described_class::Base) }
+end
+
+RSpec.describe RubyArguments::NullArguments do
+  let(:null_arguments) { described_class.new }
+
+  example_group "inheritance" do
+    specify { expect(described_class.superclass).to eq(RubyArguments) }
+  end
+
+  example_group "instance methods" do
+    describe "#args" do
+      it "returns empty array" do
+        expect(null_arguments.args).to eq([])
+      end
+    end
+
+    describe "#kwargs" do
+      it "returns empty hash" do
+        expect(null_arguments.kwargs).to eq({})
+      end
+    end
+
+    describe "#block" do
+      it "returns `nil`" do
+        expect(null_arguments.block).to be_nil
+      end
+    end
+
+    describe "#null_arguments?" do
+      it "returns `true`" do
+        expect(null_arguments.null_arguments?).to eq(true)
+      end
+    end
+  end
 end
